07754ac1963ccb1ad19c97a155967158e6bde966
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-02-12 17:18:36 +1000

    Fix #1 - Initial site skeleton

---
+++ source/Glimpse.Site/Framework/WebPageBaseExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+
+namespace Glimpse.Site.Framework
+{
+    public static class WebPageBaseExtensions
+    {
+        public static HelperResult RenderSection(this WebPageBase webPage, string name, Func<dynamic, HelperResult> defaultContents)
+        {
+            if (webPage.IsSectionDefined(name))
+            {
+                return webPage.RenderSection(name);
+            }
+            return defaultContents(null);
+        }
+    }
+}
