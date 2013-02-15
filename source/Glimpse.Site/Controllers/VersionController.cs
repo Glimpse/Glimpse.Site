564884d9fdb4967262310402b5ff90b25da5cf26
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-02-15 23:46:19 +1000

    Initial cut of the version pages

---
+++ source/Glimpse.Site/Controllers/VersionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Glimpse.Site.Controllers
+{
+    public class VersionController : Controller
+    {
+        public ActionResult Install()
+        {
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        } 
+    }
+}
