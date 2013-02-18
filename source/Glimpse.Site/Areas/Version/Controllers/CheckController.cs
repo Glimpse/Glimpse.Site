0c8f3f026a4dfba740fcbf2a134f81941a363a8e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-02-19 08:12:47 +1000

    Remove unused code

--- source/Glimpse.Site/Areas/Version/Controllers/CheckController.cs
+++ source/Glimpse.Site/Areas/Version/Controllers/CheckController.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
+﻿using System.Web.Mvc;
 using Glimpse.Package;
-using Glimpse.Site.Framework;
-
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace Glimpse.Site.Areas.Version.Controllers
 {
@@ -32,28 +23,5 @@ namespace Glimpse.Site.Areas.Version.Controllers
 
             return View(result);
         }
-
-        //public ActionResult Query(VersionCheckDetails details, bool withDetails)
-        //{
-        //    var service = PackageSettings.Settings.NewReleaseService;
-        //    var result = service.GetLatestReleaseInfo(details, withDetails);
-
-        //    result.ViewLink = GenerateViewUri(ControllerContext.HttpContext.Request.Url, result);
-
-        //    return new JsonpResult(result);
-        //}
-
-        //private string GenerateViewUri(Uri uri, LatestReleaseInfo result)
-        //{
-        //    var queryString = "";
-        //    var spacer = "";
-        //    foreach (var item in result.Details)
-        //    {
-        //        queryString += string.Format("{0}{1}={2}", spacer, item.Key, item.Value.Version);
-        //        spacer = "&";
-        //    }
-
-        //    return String.Format("{0}://{1}/release/check/details?{2}", uri.Scheme, uri.Authority, queryString);
-        //}
     }
 }
