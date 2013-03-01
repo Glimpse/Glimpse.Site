f54439ab7b4713261472ff07ca941667751b3d7f
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-03-01 21:22:19 +1000

    Create placeholder page for docs till we get actual pages

--- source/Glimpse.Site/Controllers/DocsController.cs
+++ source/Glimpse.Site/Controllers/DocsController.cs
@@ -7,10 +7,12 @@ namespace Glimpse.Site.Controllers
         public virtual ActionResult Index(string mdSlug, string controller = null)
         {
             //if (string.IsNullOrEmpty(mdSlug))
-                return Content("Help placeholder!");
+            //    return Content("Help placeholder!");
             // TODO : Provide real content!
 
             //return View(new DocumentationViewModel(mdSlug, "Views/" + controller + "/Wiki/"));
+
+            return View();
         }
     }
 }
