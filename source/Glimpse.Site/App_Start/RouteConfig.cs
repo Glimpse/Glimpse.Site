c94a64d185f3acd989915c78362497801e256027
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2013-03-01 11:51:37 +1000

    Adjust routes given new controller structure

--- source/Glimpse.Site/App_Start/RouteConfig.cs
+++ source/Glimpse.Site/App_Start/RouteConfig.cs
@@ -9,19 +9,7 @@ namespace Glimpse.Site
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Talk",
-                url: "Talk",
-                defaults: new { controller = "Home", action = "Talk" }
-            );
-
-            routes.MapRoute(
-                name: "Protocol",
-                url: "Protocol",
-                defaults: new { controller = "Home", action = "Protocol" }
-            );
-
+             
             routes.MapRoute(
                 name: "Documentation",
                 url: "Help/{mdSlug}",
