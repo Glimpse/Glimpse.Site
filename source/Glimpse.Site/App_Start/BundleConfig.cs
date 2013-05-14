94a93bb840845868ad5a8a80d5f64b024a3e350c
Pete Hotchkin <pete.hotchkin@gmail.com>
2013-05-14 09:55:48 +0100

    Removed the logo from the menu on the home page
    Also removed an image that is no longer used.

--- source/Glimpse.Site/App_Start/BundleConfig.cs
+++ source/Glimpse.Site/App_Start/BundleConfig.cs
@@ -18,7 +18,7 @@ namespace Glimpse.Site
             //            "~/Scripts/jquery.unobtrusive*",
             //            "~/Scripts/jquery.validate*"));
 
-            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            ///// Use the development version of Modernizr to develop with and learn from. Then, when you're
             //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
             //            "~/Scripts/modernizr-*"));
