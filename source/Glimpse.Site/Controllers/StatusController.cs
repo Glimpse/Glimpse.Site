8d089971a4c3261c48f84eafdba8264a07706bad
bholdt <bm.holdt@gmail.com>
2014-01-03 07:29:50 +0200

    Cleaned up Status Controller

--- source/Glimpse.Site/Controllers/StatusController.cs
+++ source/Glimpse.Site/Controllers/StatusController.cs
@@ -12,13 +12,12 @@ namespace Glimpse.Site.Controllers
 {
     public class StatusController : Controller
     {
-        private readonly GlimpsePackageViewModelMapper _glimpsePackageViewModelMapper = new GlimpsePackageViewModelMapper();
-        private GithubMilestoneService _githubMilestoneService;
+        private readonly StatusDashboardProvider _statusDashboardProvider = new StatusDashboardProvider();
 
         [OutputCache(Duration = 30 * 60)]
         public ActionResult Index()
         {
-            var statusView = SetupStatusDashboard();
+            var statusView = _statusDashboardProvider.SetupStatusDashboard(Server.MapPath("~/Content/packages.json"));
             return View(statusView);
         }
 
@@ -26,32 +25,6 @@ namespace Glimpse.Site.Controllers
         {
             Response.RemoveOutputCacheItem(Url.Action("index"));
             return RedirectToAction("Index");
-        }
-
-        private IssuesIndexViewModel SetupStatusDashboard()
-        {
-            var packageIssueProvider = CreatePackageIssueProvider();
-            var milestone = _githubMilestoneService.GetMilestone("vnext");
-            var glimpsePackages = packageIssueProvider.GetPackageIssues(milestone.Number).ToList();
-            var numberOfIssues = glimpsePackages.Sum(g => g.Issues.Count);
-            if (numberOfIssues == 0)
-            {
-                milestone = _githubMilestoneService.GetLatestMilestoneWithIssues("closed");
-                glimpsePackages = packageIssueProvider.GetPackageIssues(milestone.Number).ToList();
-            }
-            var statusView = _glimpsePackageViewModelMapper.ConvertToIndexViewModel(glimpsePackages.ToList());
-            statusView.CurrentMilestone = milestone;
-            return statusView;
-        }
-
-        private PackageIssueProvider CreatePackageIssueProvider()
-        {
-            var jsonFile = Server.MapPath("~/Content/packages.json");
-            string githubKey = ConfigurationManager.AppSettings.Get("GithubKey");
-            string githubSecret = ConfigurationManager.AppSettings.Get("GithubSecret");
-            var httpClient = new HttpClientFactory().CreateHttpClient(githubKey, githubSecret);
-            _githubMilestoneService = new GithubMilestoneService(httpClient);
-            return new PackageIssueProvider(new PackageRepository(jsonFile), new IssueRepository(new GithubIssueService(httpClient), _githubMilestoneService), _githubMilestoneService);
         }
     }
 }
