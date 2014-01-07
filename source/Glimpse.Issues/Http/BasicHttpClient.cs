62d8ee9e07af45dc0f8e6a1cd528da0983a5c804
bholdt <bm.holdt@gmail.com>
2014-01-07 14:45:47 +0200

    Added User Agent to default header for Basic Http Client

--- source/Glimpse.Issues/Http/BasicHttpClient.cs
+++ source/Glimpse.Issues/Http/BasicHttpClient.cs
@@ -13,6 +13,7 @@ namespace Glimpse.Infrastructure.Http
         public BasicHttpClient(string baseAddress, string mediaType)
         {
             _httpClient = new HttpClient() {BaseAddress = new Uri(baseAddress)};
+            _httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Glimpse.Site","v1.0"));
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(mediaType));
         }
 
