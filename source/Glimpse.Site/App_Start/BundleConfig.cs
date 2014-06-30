12e3b3bf4984564277a26c439127586e03cf81e9
Bjorn Holdt <bm.holdt@gmail.com>
2014-06-30 19:52:43 +1000

    Split out responsive styles into own file

--- source/Glimpse.Site/App_Start/BundleConfig.cs
+++ source/Glimpse.Site/App_Start/BundleConfig.cs
@@ -19,6 +19,7 @@ namespace Glimpse.Site
              
             bundles.Add(new StyleBundle("~/content/sitecss").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/reponsive.css",
                       "~/Content/site.css"));
         }
     }
